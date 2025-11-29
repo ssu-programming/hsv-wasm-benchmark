fbd4733a4c9e32d770cce36389ded7ae0b5d4c64
nayoon030303 <kny030303khs@gmail.com>
2025-11-29 15:05:36 +0900

    docs: README에 . 가산점 항목 추가

--- vite.config.ts
+++ vite.config.ts
@@ -17,5 +17,11 @@ export default defineConfig({
       'Cross-Origin-Embedder-Policy': 'require-corp',
       'Cross-Origin-Opener-Policy': 'same-origin',
     },
+    watch: {
+      ignored: ['**/emsdk/**', '**/node_modules/**'],
+    },
+  },
+  optimizeDeps: {
+    exclude: ['emsdk'],
   },
 })
